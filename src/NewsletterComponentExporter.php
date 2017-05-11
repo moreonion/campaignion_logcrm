cdde4edf7a78d1851a6975525b89eb73b15cef73
Roman Zimmermann <roman@more-onion.com>
2017-05-11 15:02:43 +0200

    Adapt to new newletter checkboxe behavior.

--- src/NewsletterComponentExporter.php
+++ src/NewsletterComponentExporter.php
@@ -5,7 +5,7 @@ namespace Drupal\campaignion_logcrm;
 class NewsletterComponentExporter {
   public function value($component, $values) {
     if ($component['extra']['display'] == 'radios') {
-      if ($values['subscribed']) {
+      if (reset($values)) {
         return $component['extra']['radio_labels'][1];
       }
       else {
@@ -13,7 +13,7 @@ class NewsletterComponentExporter {
       }
     }
     else {
-      if ($values['subscribed']) {
+      if (reset($values)) {
         return $component['extra']['checkbox_label'];
       }
       else {
