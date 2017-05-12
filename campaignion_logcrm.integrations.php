084a90f9a67353e3f04d1dc092d77bafb3ceafad
Roman Zimmermann <roman@more-onion.com>
2017-05-12 12:32:09 +0200

    Add paypal data to payment events.

--- campaignion_logcrm.integrations.php
+++ campaignion_logcrm.integrations.php
@@ -7,6 +7,9 @@
  */
 
 use \Drupal\manual_direct_debit_uk\AccountDataController;
+use \PayPalPaymentECPaymentMethodController as PayPalECController;
+use \PayPalPaymentPPSPaymentMethodController as PayPalPPSController;
+use \PayPalPaymentIPNController as PayPalIPNController;
 
 /**
  * Implements hook_campaignion_logcrm_payment_event_data_alter().
@@ -20,5 +23,18 @@ function manual_direct_debit_uk_campaignion_logcrm_payment_event_data_alter(arra
       'sort_code' => $md['bank_code'],
       'payment_date' => $md['payment_date'],
     ];
+  }
+}
+
+/**
+ * Implements hook_campaignion_logcrm_payment_event_data_alter().
+ */
+function paypal_payment_campaignion_logcrm_payment_event_data_alter(array &$data, Payment $payment) {
+  $controller = $payment->method->controller;
+  if ($controller instanceof PaypalECController || $controller instanceof PayPalPPSController) {
+    $status = $payment->getStatus();
+    if ($ipn = PayPalIPNController::load($status->psiid)) {
+      $data['transaction_id'] = $ipn->txn_id;
+    }
   }
 }
