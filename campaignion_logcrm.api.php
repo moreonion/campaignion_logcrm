8a55c803d0f1d7882db89d0d649312806b086945
Roman Zimmermann <roman@more-onion.com>
2019-04-04 13:05:50 +0200

    Add more general hook for modifying event data.

--- campaignion_logcrm.api.php
+++ campaignion_logcrm.api.php
@@ -8,6 +8,23 @@
  */
 
 /**
+ * Allow other modules to modify the event data.
+ *
+ * @param array $data
+ *   Original event data array.
+ * @param string $type
+ *   The event type.
+ * @param array $context
+ *   Array of objects used to generate the event data.
+ */
+function hook_campaignion_logcrm_payment_event_data_alter(array &$data, $type, array $context) {
+  if (isset($context['submission'])) {
+    $link_options = ['absolute' => TRUE, 'alias' => TRUE];
+    $data['_link']['edit'] = url("node/{$submission->node->nid}/submission/{$submission->sid}/edit", $link_options);
+  }
+}
+
+/**
  * Allow other modules to modify the event data for payment events.
  *
  * @param array $data
--- src/Event.php
+++ src/Event.php
@@ -11,13 +11,31 @@ class Event {
 
   public static function fromSubmission(Submission $submission, $type = 'form_submission') {
     $data = Loader::instance()->submissionExporter()->data($submission);
-    return new static($type, $submission->submitted, $data);
+    $context['submission'] = $submission;
+    return static::fromData($type, $submission->submitted, $data, $context);
   }
 
   public static function fromSubmissionConfirmation(Submission $submission, $type = 'form_submission_confirmed') {
-    return new static($type, time(), [
-      'uuid' => $submission->uuid,
-    ]);
+    $data = ['uuid' => $submission->uuid];
+    $context['submission'] = $submission;
+    return static::fromData($type, time(), $data, $context);
+  }
+
+  /**
+   * Create new event by passing data and context.
+   *
+   * @param string $type
+   *   Event type.
+   * @param int $time
+   *   The point in time the event was triggerd.
+   * @param array $data
+   *   The generated event data.
+   * @param array $context
+   *   An array of data used as source for generating the event data.
+   */
+  public static function fromData($type, $time, array $data, array $context) {
+    drupal_alter('campaignion_logcrm_event_data', $data, $type, $context);
+    return new static($type, $time, $data);
   }
 
   public static function fromPayment(\Payment $payment, $type = 'payment_success') {
@@ -32,9 +50,12 @@ class Event {
     $data['method_specific'] = $payment->method->title_specific;
     $data['method_generic'] = $payment->method->title_generic;
     $data['controller'] = $payment->method->controller->name;
+
     // Let other modules alter the data.
     drupal_alter('campaignion_logcrm_payment_event_data', $data, $payment);
-    return new static($type, $status->created, $data);
+    $context['payment'] = $payment;
+    $context['submission'] = $submission_obj;
+    return static::fromData($type, $status->created, $data, $context);
   }
 
   public function __construct($type, $date = NULL, $data = []) {
